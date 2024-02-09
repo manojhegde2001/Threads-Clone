c695ce161cb7025dfaedc8ff5ec9fe5db03ff227
Manoj Hegde <manojhegde2001@gmail.com>
2024-02-09 11:27:29 +0530

    files

---
+++ app/(roots)/page.tsx
@@ -0,0 +1,10 @@
+// import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <h1 className="head-text text-left">
+      {/* <UserButton afterSignOutUrl="/" /> */}
+      Home
+    </h1>
+  )
+}
